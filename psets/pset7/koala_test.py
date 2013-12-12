3afbb3756feb6b882e34bf05be7416f4346bd24a
pclark10 <patricia.clark.09@gmail.com>
2013-12-12 09:25:41 -0500

    pset7 koala test update

--- psets/pset7/koala_test.py
+++ psets/pset7/koala_test.py
@@ -2,16 +2,16 @@ from ps7 import *
 
 triggerMap = {}
 
-koala = NewsStory('', 'Koala bears are softand cuddly', '', '', '')
+koalastory = NewsStory('', 'Koala bears are softand cuddly', '', '', '')
 
-#s2  = TitleTrigger('')
-#s3 = TitleTrigger('koala')
+s2  = TitleTrigger('')
+s3 = TitleTrigger('koala')
 #print s2.evaluate(koala)
 #
 #s4 = OrTrigger(s2, s3)
 #print s4.evaluate(koala)
 
-trigger = makeTrigger(triggerMap, 'TITLE', 'koala', 't1')
+#trigger = makeTrigger(triggerMap, 'TITLE', 'koala', 't1')
 
 #s5 = PhraseTrigger('Koala bears are soft and')
-print trigger.evaluate(koala)
+print s3.evaluate(koalastory)
